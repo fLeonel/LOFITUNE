17aeecf500bd640ae64d9a3758cad1436ceefca0
Dennis <dr04860@gmail.com>
2024-03-05 11:09:28 -0600

    cree casi todo el front SIN dinamismo

---
+++ app/pages/login/page.tsx
@@ -0,0 +1,14 @@
+export default function Page() {
+    return (
+      <nav>
+        <h1>
+            NextGoogle
+        </h1>
+        <div>
+            <form action="">
+                <input type="" id="" name="" />
+            </form>
+        </div>
+      </nav>
+    )
+}
